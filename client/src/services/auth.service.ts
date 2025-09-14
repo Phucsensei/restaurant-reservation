4dad7506debf77fc658a08a2334e470adabd7e77
phuc <phuct6776@gmail.com>
2025-09-14 11:32:36 +0700

    fix: logic code .env

--- client/src/services/auth.service.ts
+++ client/src/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { AuthResponse, LoginCredentials, RegisterCredentials } from '@/types/auth'
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/api'
+const API_URL = process.env.NEXT_PUBLIC_API_URL
 
 export const authService = {
   async login(credentials: LoginCredentials): Promise<AuthResponse> {
